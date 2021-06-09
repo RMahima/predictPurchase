71d590873f1d2d26c438a477649c1e580a3a878a
RMahima <43634959+RMahima@users.noreply.github.com>
2021-06-09 17:39:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('CustomerCategoryFeatureMod.ft', 'wb'))
+model = pickle.load(open('saved_model', 'wb'))
 
 @app.route('/')
 def home():
